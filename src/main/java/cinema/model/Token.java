8db85d76618bdcf2454bcc15be5de2a7014f5055
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-12-25 21:54:23 +0500

    Create a new constructor for Jackson. Override equals and hashCode in Token.java

--- src/main/java/cinema/model/Token.java
+++ src/main/java/cinema/model/Token.java
@@ -1,5 +1,6 @@
 package cinema.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonGetter;
 
 import java.util.UUID;
@@ -10,6 +11,10 @@ public class Token {
     public Token(UUID token) {
         this.token = token;
     }
+    @JsonCreator
+    public Token(String token){
+        this.token = UUID.fromString(token);
+    }
 
     @JsonGetter
     public UUID getToken() {
@@ -18,5 +23,18 @@ public class Token {
 
     public void setToken(UUID token) {
         this.token = token;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Token token1)) return false;
+
+        return this.getToken().toString().equals(token1.getToken().toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return getToken().hashCode();
     }
 }
